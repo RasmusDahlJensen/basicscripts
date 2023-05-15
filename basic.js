caa072f9bf6061898fcc16f00c6af2e048b1ea9a
Rasmus Jensen <rasmusdj96@gmail.com>
2023-05-15 08:44:13 +0200

    initial commit

--- basic.js
+++ basic.js
@@ -1,56 +1,31 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const fruits = ["apple", "cherry", "banana", "date"];
+const numbers = [15, 25, 35, 45, 55, 65, 75, 85, 95];
+const fruits = ["grapefruit", "lemon", "lime", "orange"];
 
-// Array.prototype.filter():
-// Creates a new array with all elements that pass a given test.
-// Opretter en ny array med alle elementer, der består en given test.
-const filteredNumbers = numbers.filter((number) => number > 2);
-console.log(filteredNumbers); // Output: [3, 4, 5, 6, 7, 8, 9]
+const filteredNumbers = numbers.filter(/* Add filter function here */);
+console.log(filteredNumbers);
 
-// Array.prototype.map():
-// Creates a new array by calling a provided function on every element in the calling array.
-// Opretter en ny array ved at kalde en givet funktion på hvert element i den kaldende array.
-const doubledNumbers = numbers.map((number) => number * 2);
-console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10 ,12 , 14 , 16, 18]
+const doubledNumbers = numbers.map(/* Add map function here */);
+console.log(doubledNumbers);
 
-// Array.prototype.reduce():
-// Applies a function against an accumulator and each element in the array to reduce it to a single value.
-// Anvender en funktion på en akkumulator og hvert element i arrayet for at reducere det til en enkelt værdi.
-const sum = numbers.reduce((acc, curr) => acc + curr, 0);
-console.log(sum); // Output: 45
+const sum = numbers.reduce(/* Add reduce function here */);
+console.log(sum);
 
-// Array.prototype.every():
-// Tests whether all elements in the array pass the provided function's test.
-// Tester om alle elementer i arrayet består af den givne funktions test.
-const allEven = numbers.every((number) => number % 2 === 0);
-console.log(allEven); // Output: false
+const allEven = numbers.every(/* Add every function here */);
+console.log(allEven);
 
-// Array.prototype.some():
-// Tests whether at least one element in the array passes the provided function's test.
-// Tester om mindst ét element i arrayet består af den givne funktions test.
-const hasEven = numbers.some((number) => number % 2 === 0);
-console.log(hasEven); // Output: true
+const hasEven = numbers.some(/* Add some function here */);
+console.log(hasEven);
 
-// Array.prototype.find():
-// Returns the first element in the array that satisfies the provided testing function.
-// Returnerer det første element i arrayet, der opfylder den givne testfunktion.
-const firstEven = numbers.find((number) => number % 2 === 0);
-console.log(firstEven); // Output: 2
+const firstEven = numbers.find(/* Add find function here */);
+console.log(firstEven);
 
-// Array.prototype.findIndex():
-// Returns the index of the first element in the array that satisfies the provided testing function.
-// Returnerer indekset for det første element i arrayet, der opfylder den givne testfunktion.
-const firstEvenIndex = numbers.findIndex((number) => number % 2 === 0);
-console.log(firstEvenIndex); // Output: 1
+const firstEvenIndex = numbers.findIndex(/* Add findIndex function here */);
+console.log(firstEvenIndex);
 
-// Array.prototype.includes():
-// Determines whether an array includes a certain element.
-// Afgør om en array indeholder et bestemt element.
-const includesThree = numbers.includes(3);
-console.log(includesThree); // Output: true
+const includesThree = numbers.includes(/* Add includes function here */);
+console.log(includesThree);
 
-// excample chaining using js on sort and map
-//eksempel på chaining sort og map
-
-const randumMap = fruits.sort(() => Math.random() - 0.5).map((fruit) => fruit);
-console.log(randumMap);
+const randomMap = fruits
+	.sort(/* Add sort function here */)
+	.map(/* Add map function here */);
+console.log(randomMap);
