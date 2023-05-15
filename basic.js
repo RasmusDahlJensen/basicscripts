337e5d1fbed17ebede5d9e6b488c4c5f9aa91658
Rasmus Jensen <rasmusdj96@gmail.com>
2023-05-15 10:37:45 +0200

    finished chains

--- basic.js
+++ basic.js
@@ -1,31 +1,50 @@
 const numbers = [15, 25, 35, 45, 55, 65, 75, 85, 95];
-const fruits = ["grapefruit", "lemon", "lime", "orange"];
 
-const filteredNumbers = numbers.filter(/* Add filter function here */);
-console.log(filteredNumbers);
+//Filters array and returns a new array with numbers greater than 50
+const filteredNumbers = numbers.filter((number) => number > 50);
+// console.log(filteredNumbers); // [ 55, 65, 75, 85, 95 ]
 
-const doubledNumbers = numbers.map(/* Add map function here */);
-console.log(doubledNumbers);
+//Map returns a new array with the result of the function being called on the array
+const multipliedNumbers = numbers.map((number) => number * 10);
+// console.log(multipliedNumbers); //[ 150, 250, 350, 450, 550, 650, 750, 850, 950 ]
 
-const sum = numbers.reduce(/* Add reduce function here */);
-console.log(sum);
+//Accumulates every number into a single value by multiplying each number with each other
+const sum = numbers.reduce((acc, curr) => acc * curr);
+// console.log(sum); // 1278767724609375
 
-const allEven = numbers.every(/* Add every function here */);
-console.log(allEven);
+//Checks if every number is less than 1000
+const allEven = numbers.every((number) => number < 1000);
+// console.log(allEven); // true
 
-const hasEven = numbers.some(/* Add some function here */);
-console.log(hasEven);
+//Checks if at least 1 number is even
+const hasEven = numbers.some((number) => number % 2 === 0);
+// console.log(hasEven); // false
 
-const firstEven = numbers.find(/* Add find function here */);
-console.log(firstEven);
+//Returns the first value that is true, in this case the first number in the array thats greater than 35
+const firstEven = numbers.find((number) => number > 35);
+// console.log(firstEven); //45
 
-const firstEvenIndex = numbers.findIndex(/* Add findIndex function here */);
-console.log(firstEvenIndex);
+//Returns the index of the first element that passes the function
+const firstEvenIndex = numbers.findIndex((number) => number > 35);
+// console.log(firstEvenIndex); // 3
 
-const includesThree = numbers.includes(/* Add includes function here */);
-console.log(includesThree);
+//Returns if any of the array indexes contain a certain value
+const includesThree = numbers.includes(15);
+// console.log(includesThree); // true
 
-const randomMap = fruits
-	.sort(/* Add sort function here */)
-	.map(/* Add map function here */);
-console.log(randomMap);
+//Chaining
+//Filter returns a new array with all numbers greater than 50 and then sums them up
+const chain1 = numbers
+	.filter((number) => number > 50)
+	.reduce((acc, cur) => acc + cur);
+// console.log(chain1); //375
+
+//Find all numbers that are divisible by 5 and then multiply it by 2
+const chain2 = numbers.find((number) => number % 5 === 0) * 2;
+// console.log(chain2); // 30
+
+//Filter our all numbers less than or even with 50 and then check if the remaining are divisible by 3
+const chain3 = numbers
+	.filter((number) => number >= 50)
+	.some((number) => number % 3 === 0);
+// console.log(chain3); // true
